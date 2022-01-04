52cecf4bfe262af0fe0d1432191c751e12c8e4a0
Softterier <43970002+Softterier@users.noreply.github.com>
2022-01-04 15:30:40 -0500

    Create Secuencia.py
    Corrección de Create

---
+++ secuencia.py
@@ -0,0 +1,10 @@
+fav_language = input("What's your favorite programming language? ")
+
+if fav_language == "Python":
+    print("Nice choice!")
+elif fav_language == "Golang":
+    print("Eres un chico con Golang, el lenguaje de programación de código abierto creado por Google. con enfoque principal es la productividad, "Go", lenguaje de programación concurrente y compilado inspirado en la sintaxis de C, que intenta ser dinámico como Python y con el rendimiento de C o C++. el lenguaje se diferencia de los demás por ser compilado y altamente escalable.")
+elif fav_language == "JavaScript":
+    print("Okay Mr. web developer.")
+else:
+    print("I don't know that language.")
