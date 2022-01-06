91371b6d79c052a0830327ffa1ecb9505b04f8dd
Softterier <43970002+Softterier@users.noreply.github.com>
2022-01-06 16:58:35 -0500

    Create Mayhem.py

---
+++ Mayhem.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#Bienvenido ap proyecto Mayhem [1.0]
+
+Print ("No eres tu trabajo")
+Print ("No eres el trabajo que tienes")
+Print ("Eres lo que Sabes")
